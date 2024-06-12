afd8aad2a5f366368a9dff146122acc3b55d53b6
Korrigan <clech.cassandra@yahoo.fr>
2024-06-12 15:53:08 +0200

    feat: add more style && category color

--- tests/unit/models/element-test.js
+++ tests/unit/models/element-test.js
@@ -25,4 +25,46 @@ module('Unit | Model | element', function (hooks) {
       'grid-row-start: 5; grid-column-start: 2',
     );
   });
+
+  // Test color property
+  test('it has a correct color', function (assert) {
+    let store = this.owner.lookup('service:store');
+    let element = store.createRecord('element', {
+      category: 'diatomic nonmetal',
+    });
+
+    assert.strictEqual(element.color, '#3FB8AF');
+
+    element.category = 'noble gas';
+
+    assert.strictEqual(element.color, '#FF3D7F');
+
+    element.category = 'alkali metal';
+
+    assert.strictEqual(element.color, '#FF4E50');
+
+    element.category = 'alkaline earth metal';
+
+    assert.strictEqual(element.color, '#FC913A');
+
+    element.category = 'metalloid';
+
+    assert.strictEqual(element.color, '#F9D423');
+
+    element.category = 'polyatomic nonmetal';
+
+    assert.strictEqual(element.color, '#EDE574');
+
+    element.category = 'post-transition metal';
+
+    assert.strictEqual(element.color, '#E1F5C4');
+
+    element.category = 'unknown';
+
+    assert.strictEqual(element.color, '#FF9E9D');
+
+    element.category = 'my-category';
+
+    assert.strictEqual(element.color, '#DAD8A7');
+  });
 });
