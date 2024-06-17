d2be422fb39287754ec948133c0c1e06c42530b7
Korrigan <clech.cassandra@yahoo.fr>
2024-06-17 16:56:08 +0200

    feat: add element details panel

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit } from '@ember/test-helpers';
+import { visit, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-periodic-table/tests/helpers';
 
 module('Acceptance | index', function (hooks) {
@@ -9,7 +9,7 @@ module('Acceptance | index', function (hooks) {
     await visit('/');
 
     assert
-      .dom('[data-test-element]')
+      .dom('.element-container')
       .exists({ count: 119 }, 'all the elements are rendered');
   });
 
@@ -18,24 +18,67 @@ module('Acceptance | index', function (hooks) {
 
     // Test first element
     assert
-      .dom('[data-test-element]:first-child [data-test-element-number]')
+      .dom('.element-container:first-child [data-test-element-number]')
       .hasText('1', 'element number is rendered');
     assert
-      .dom('[data-test-element]:first-child [data-test-element-symbol]')
+      .dom('.element-container:first-child [data-test-element-symbol]')
       .hasText('H', 'element symbol is rendered');
     assert
-      .dom('[data-test-element]:first-child [data-test-element-name]')
+      .dom('.element-container:first-child [data-test-element-name]')
       .hasText('Hydrogen', 'element name is rendered');
 
     // Test another element
     assert
-      .dom('[data-test-element]:nth-child(107) [data-test-element-number]')
+      .dom('.element-container:nth-child(107) [data-test-element-number]')
       .hasText('107', 'element number is rendered');
     assert
-      .dom('[data-test-element]:nth-child(107) [data-test-element-symbol]')
+      .dom('.element-container:nth-child(107) [data-test-element-symbol]')
       .hasText('Bh', 'element symbol is rendered');
     assert
-      .dom('[data-test-element]:nth-child(107) [data-test-element-name]')
+      .dom('.element-container:nth-child(107) [data-test-element-name]')
       .hasText('Bohrium', 'element name is rendered');
+  });
+
+  test('it displays element details', async function (assert) {
+    await visit('/');
+
+    // Click on the first element
+    await click('.element-container:first-child [data-test-element]');
+
+    assert
+      .dom('[data-test-element-details]')
+      .exists('decorative element is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-symbol]')
+      .hasText('H', 'element symbol is rendered');
+    assert
+      .dom('.element-details [data-test-element-name]')
+      .hasText('Hydrogen', 'element name is rendered');
+    assert
+      .dom('.element-details [data-test-element-atomic-mass]')
+      .hasText('1.008', 'element atomic mass is rendered');
+    assert
+      .dom('.element-details [data-test-element-summary]')
+      .exists('element summary is rendered');
+
+    // Switch element
+    await click('.element-container:nth-child(107) [data-test-element]');
+
+    assert
+      .dom('.element-details [data-test-element-symbol]')
+      .hasText('Bh', 'element symbol is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-name]')
+      .hasText('Bohrium', 'element name is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-atomic-mass]')
+      .hasText('270', 'element atomic mass is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-summary]')
+      .exists('element summary is rendered');
   });
 });
