f2bc876f06c1c6324f94f942b841b04c6597bcc2
Korrigan <clech.cassandra@yahoo.fr>
2024-06-17 17:12:15 +0200

    feat: add more details

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -55,12 +55,42 @@ module('Acceptance | index', function (hooks) {
     assert
       .dom('.element-details [data-test-element-name]')
       .hasText('Hydrogen', 'element name is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-number]')
+      .hasText('1', 'element number is rendered');
+
     assert
       .dom('.element-details [data-test-element-atomic-mass]')
       .hasText('1.008', 'element atomic mass is rendered');
+
     assert
-      .dom('.element-details [data-test-element-summary]')
-      .exists('element summary is rendered');
+      .dom('.element-details [data-test-element-category]')
+      .hasText('diatomic nonmetal', 'element category is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-phase]')
+      .hasText('Gas', 'element phase is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-boiling-point]')
+      .hasText('20.271 K', 'element boiling point is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-melting-point]')
+      .hasText('13.99 K', 'element melt is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-density]')
+      .hasText('0.08988 kg/m^3', 'element density is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-discovered-by]')
+      .hasText('Henry Cavendish', 'element discovered by is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-named-by]')
+      .hasText('Antoine Lavoisier', 'element named by is rendered');
 
     // Switch element
     await click('.element-container:nth-child(107) [data-test-element]');
@@ -74,11 +104,42 @@ module('Acceptance | index', function (hooks) {
       .hasText('Bohrium', 'element name is rendered');
 
     assert
+      .dom('.element-details [data-test-element-number]')
+      .hasText('107', 'element number is rendered');
+
+    assert
       .dom('.element-details [data-test-element-atomic-mass]')
       .hasText('270', 'element atomic mass is rendered');
 
     assert
-      .dom('.element-details [data-test-element-summary]')
-      .exists('element summary is rendered');
+      .dom('.element-details [data-test-element-category]')
+      .hasText('transition metal', 'element category is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-phase]')
+      .hasText('Solid', 'element phase is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-boiling-point]')
+      .doesNotExist('element boiling point is not rendered');
+
+    assert
+      .dom('.element-details [data-test-element-melt]')
+      .doesNotExist('element melt is not rendered');
+
+    assert
+      .dom('.element-details [data-test-element-density]')
+      .hasText('37.1 kg/m^3', 'element density is rendered');
+
+    assert
+      .dom('.element-details [data-test-element-discovered-by]')
+      .hasText(
+        'Gesellschaft für Schwerionenforschung',
+        'element discovered by is rendered',
+      );
+
+    assert
+      .dom('.element-details [data-test-element-named-by]')
+      .doesNotExist('element named by is not rendered');
   });
 });
